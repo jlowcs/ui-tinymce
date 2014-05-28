a39ae70c3b3c16216a8dee9fdfb3163ee9c72094
Jerome Louis <j.louis@oodrive.fr>
2014-05-28 15:35:31 +0100

    fix initial firty state for linefeeds

--- src/tinymce.js
+++ src/tinymce.js
@@ -55,7 +55,7 @@ angular.module('ui.tinymce', [])
             });
             // Update model on change, i.e. copy/pasted text, plugins altering content
             ed.on('SetContent', function (e) {
-              if (!e.initial && ngModel.$viewValue !== e.content) {
+              if (!e.initial && (ngModel.$viewValue || '') !== (ed.getContent() || '')) {
                 ed.save();
                 updateView();
               }
